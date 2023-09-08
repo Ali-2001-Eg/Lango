3fdacdda059d05c091033f719745bf4752a961a8
Ali-2001-Eg <AliBusiness2001@outlook.com>
2023-09-08 20:23:07 +0300

    Adding grouping and calling features

---
+++ lib/screens/call/call_pickup_screen.dart
@@ -0,0 +1,128 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:whatsapp_clone/controllers/call_controller.dart';
+import 'package:whatsapp_clone/shared/utils/base/error_screen.dart';
+import 'package:whatsapp_clone/shared/widgets/custom_indicator.dart';
+
+import '../../models/call_model.dart';
+import 'call_screen.dart';
+
+class CallPickupScreen extends ConsumerWidget {
+  final Widget scaffold;
+  // final String receiverName;
+  
+  CallPickupScreen({
+    required this.scaffold,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return StreamBuilder<DocumentSnapshot>(
+        stream: ref.read(callControllerProvider).chatStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return ErrorScreen(error: snapshot.error.toString());
+          } else if (snapshot.hasData && snapshot.data!.data() != null) {
+            CallModel callData = CallModel.fromJson(
+                snapshot.data!.data() as Map<String, dynamic>);
+            if (!callData.hasDialled) {
+              return Scaffold(
+                body: Container(
+                  //alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text(
+                        'Incoming Call',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(callData.callerPic),
+                        radius: 150,
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        callData.callerName,
+                        style: const TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            InkWell(
+                                highlightColor: Colors.grey,
+                                splashColor: Colors.transparent,
+                                borderRadius: BorderRadius.circular(50),
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.all(15),
+                                  decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.white10),
+                                  child: const Icon(
+                                    Icons.call_end,
+                                    size: 35,
+                                    color: Colors.redAccent,
+                                  ),
+                                )),
+                            InkWell(
+                                borderRadius: BorderRadius.circular(50),
+                                highlightColor: Colors.grey,
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => CallScreen(
+                                        channelId: callData.callId,
+                                        callData: callData,
+                                        isGroupChat: false,
+                                      ),
+                                    ),
+                                  );
+                                },
+
+                                child: Container(
+                                  padding: const EdgeInsets.all(15),
+                                  decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.white10),
+                                  child: const Icon(
+                                    Icons.call,
+                                    size: 35,
+                                    color: Colors.greenAccent,
+                                  ),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+          }
+          return scaffold;
+        });
+  }
+}
---
+++ lib/screens/call/call_screen.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/controllers/call_controller.dart';
+
+import 'package:whatsapp_clone/models/call_model.dart';
+import 'package:whatsapp_clone/shared/utils/base/agora_config.dart';
+import 'package:whatsapp_clone/shared/widgets/custom_indicator.dart';
+
+class CallScreen extends ConsumerStatefulWidget {
+  final String channelId;
+  final CallModel callData;
+  final bool isGroupChat;
+  const CallScreen({
+    super.key,
+    required this.channelId,
+    required this.callData,
+    required this.isGroupChat,
+  });
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CallScreenState();
+}
+
+class _CallScreenState extends ConsumerState<CallScreen> {
+//agora setup
+  AgoraClient? client;
+
+  @override
+  void initState() {
+    super.initState();
+    client = AgoraClient(
+      agoraConnectionData: AgoraConnectionData(
+        appId: AgoraConfig.appId,
+        channelName: widget.channelId,
+        tokenUrl: AgoraConfig.baseUrl,
+      ),
+    );
+    initAgora();
+  }
+
+  void initAgora() async {
+    await client!.initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: client == null
+          ? const CustomIndicator()
+          : SafeArea(
+              child: Stack(
+                children: [
+                  AgoraVideoViewer(client: client!),
+                  AgoraVideoButtons(
+                    client: client!,
+                    disconnectButtonChild: InkWell(
+                        highlightColor: Colors.grey,
+                        splashColor: Colors.transparent,
+                        borderRadius: BorderRadius.circular(50),
+                        onTap: () {
+                          client!.engine.leaveChannel();
+                          ref.read(callControllerProvider).endCall(
+                              callerId: widget.callData.callerId,
+                              receiverId: widget.callData.receiverId,
+                              context: context,
+                              isGroupChat: widget.isGroupChat);
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.white),
+                          child: const Icon(
+                            Icons.call_end,
+                            size: 23,
+                            color: Colors.redAccent,
+                          ),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
